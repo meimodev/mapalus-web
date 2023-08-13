6f0c849c84300cbebc201573f972ff6b3615fb79
meimodev <meimodev@gmail.com>
2023-08-13 13:16:37 +0700

    added activid-bppd page

---
+++ pages/activid-bppd/index.jsx
@@ -0,0 +1,48 @@
+/* eslint-disable @next/next/no-img-element */
+import FooterDefault from "../../comps/FooterDefault";
+import {Fade} from "react-awesome-reveal";
+import Meta from "../../comps/Meta";
+
+export default function Page() {
+    // noinspection HtmlUnknownTarget
+
+    const images = [
+        "https://i.ibb.co/bmHfFQF/2.jpg",
+        "https://i.ibb.co/mqdnp2R/3.jpg",
+        "https://i.ibb.co/jJ72Vgv/4.jpg",
+        "https://i.ibb.co/hLV72D3/5.jpg",
+        "https://i.ibb.co/fSz6JFf/6.jpg",
+        "https://i.ibb.co/z6tWVyL/7.jpg",
+        "https://i.ibb.co/RDxF9hJ/8.jpg",
+    ];
+
+    return (
+        <div className="bg-[#ede4d7] text-red-600 px-6 py-6">
+            <Meta
+                title="Activid - BPPD Minahasa"
+                description="Activid partership agreement with Badan Promosi Pariwisata Daerah kabupaten Minahasa"
+                color="#ede4d7"
+                image="/mapalus.svg"
+            />
+
+            <div className="max-w-lg mx-auto px-6 py-6">
+                <div className="text-center text-xl text-stone-400 pb-6 font-thin">
+                    Activid <br/>x<br/> BPPD Minahasa 2023
+                </div>
+                <Fade cascade triggerOnce>
+                    {images.map((e,i)=>
+                        <div key={e} className="border-l-2 border-r-2 border-stone-400 px-6 mb-10">
+
+                        <img  alt="image" src={images[i]} />
+                        </div>
+                    )}
+                </Fade>
+                <div className="mx-auto w-[3rem] mt-10 mb-14">
+
+                <FooterDefault/>
+
+                </div>
+            </div>
+        </div>
+    );
+}
