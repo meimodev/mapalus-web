d145183b0478e3e4a27233bcd271f8d04a94dfa8
meimo <59273253+meimodev@users.noreply.github.com>
2025-08-01 19:05:46 +0800

    fix bol-bol landing page, only the first page

--- pages/bol-bol-studio/home.jsx
+++ pages/bol-bol-studio/home.jsx
@@ -3,20 +3,43 @@ import React from 'react';
 const HomeView = ({onClickBooking}) => {
 
     return (
-        <div className="p-4 bg-gray-900 text-white h-screen flex flex-col  justify-center ">
+        <div className="bg-blue-800 text-white h-screen flex flex-col  justify-center ">
 
-            <div className="text-center">
-                <h2 className="text-2xl font-bold ">Bol bol Studio</h2>
+            {/*<img src="https://ik.imagekit.io/geb6bfhmhx/bol-bol-studio/main.jpg?updatedAt=1754041084936" alt="main image"*/}
+            {/*/>*/}
 
-                <div className="py-2">
-                    Everyday 11:00 - 22:00
+            <div
+                className=" bg-[url('https://ik.imagekit.io/geb6bfhmhx/bol-bol-studio/main.jpg?updatedAt=1754041084936')] w-full h-full mx-auto bg-contain bg-center bg-no-repeat relative">
+
+                <div
+                    className="text-center bottom-24 left-0 right-0 absolute bg-gradient-to-t from-blue-800 via-blue-800 to-transparent ">
+
+                    <div className="pt-12 font-bold">
+                        <div className="text-5xl font-lemon">
+                            EVERYDAY
+                        </div>
+                        <div className="text-xl">
+                            11:00 AM - 22:00 PM
+                        </div>
+                    </div>
+
+                    <div className="flex justify-around font-poppins pt-4 pb-4 font-bold">
+                        <a href="https://www.instagram.com/bolbol_studio" target="_blank" className='flex gap-1 items-center'>
+                            <img src="/logos/logo-instagram.svg" className="w-6 h-6" alt=""/>  @bolbol_studio
+                        </a>
+                        <a href="https://www.tiktok.com/" target="_blank" className='flex gap-1 items-center'>
+                            <img src="/logos/logo-tiktok.svg" className="w-6 h-6" alt=""/>  @bolbol_studio
+                        </a>
+
+                    </div>
+
+                    <button
+                        onClick={onClickBooking}
+                        className="text-blue-800 px-8 py-4 rounded-2xl bg-white mt-4 font-black">
+                        BOOK NOW
+                    </button>
+
                 </div>
-
-                <button
-                    onClick={onClickBooking}
-                    className="text-white px-4 py-2 rounded-2xl bg-blue-600 mt-4">
-                    BOOK NOW
-                </button>
             </div>
         </div>
     );
