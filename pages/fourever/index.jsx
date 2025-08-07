5eaa418dd45d5324eb286f98fb58b6e9cb787d9b
meimo <59273253+meimodev@users.noreply.github.com>
2025-08-07 13:18:54 +0800

    fix fourever page

--- pages/fourever/index.jsx
+++ pages/fourever/index.jsx
@@ -9,7 +9,7 @@ export default function Fourever() {
         title="Fourever Gift | Tondano"
         description="Fourever Gift | Tondano"
         color="#1c1917"
-        image={imageUrlList[0]}
+        // image={imageUrlList[0]}
     >
         <link
             rel="stylesheet"
@@ -26,61 +26,9 @@ export default function Fourever() {
             rel="stylesheet"
         />
     </Meta>
-    const primaryFontFamily = {fontFamily: "'Dancing Script', cursive"};
-    const secondaryFontFamily = {fontFamily: "'Caveat', cursive"};
-    const thirtiaryFontFamily = {fontFamily: "'Mukta', sans-serif"};
-
-    const imageUrlList = [
-        "https://i.ibb.co/TgNRZHR/product-1-acrylic-color.png",
-        "https://i.ibb.co/8K77NpN/product-2-acrylic-LED.png",
-        "https://i.ibb.co/ZJybk8m/product-3-giftbox.png",
-        "https://i.ibb.co/S62tff1/product-4-bouquet.png",
-        "https://i.ibb.co/zmhKvZK/element-1.png",
-        "https://i.ibb.co/G3qgTdh/element-2.png",
-        "https://i.ibb.co/Q86MWD8/element-3.png",
-        "https://i.ibb.co/BB6T0y2/circle.png",
-    ]
-
-    const SideBySideSectionItem = ({price, title, description, imageUrl, decoration}) => {
-        return <div className="flex gap-4 relative">
-            <img
-                src={imageUrl}
-                className="h-32 xs:h-52" alt="Image1"/>
-
-            <div className="self-center mr-4 z-10">
-                <h4 className="text-3xl font-bold pb-2" style={secondaryFontFamily}>
-                    {title}
-                </h4>
-                <div style={thirtiaryFontFamily} className="text-xs text-slate-400">
-                    {description.map(e => <div key={e}>{e}</div>)}
-                </div>
-                <div className="font-bold py-1">
-                    {price}
-                </div>
-                <div className="py-2 ">
-                    <button
-                        onClick={(e)=>{
-                            e.preventDefault();
-                            const waUrl = `https://api.whatsapp.com/send?phone=6287865938722&text=Halo%2C%20Tolong%20info%20pemesanan%20produk%20-%20${title}`;
-                            window.open(waUrl,"_blank")
-                        }}
-                        className="bg-sky-400 text-white text-sm rounded-tl-lg rounded-bl-lg rounded-tr-lg flex py-1 items-center">
-                        <div className="px-2">
-                            <i className="las la-plus text-lg"></i>
-                        </div>
-                        <div style={thirtiaryFontFamily} className="pr-3 text-sm ">
-                            Pesan
-                        </div>
-                    </button>
-                </div>
-
-            </div>
-            {decoration}
-        </div>;
-    }
 
     const _ClosingListItem = ({title, icon, link}) => <button
-        onClick={e=>{
+        onClick={e => {
             e.preventDefault();
             window.open(link, "_blank");
         }}
@@ -91,114 +39,65 @@ export default function Fourever() {
         </div>
     </button>
 
+    const primaryFontFamily = {fontFamily: "'Dancing Script', cursive"};
+    const secondaryFontFamily = {fontFamily: "'Caveat', cursive"};
+    const thirdFontFamily = {fontFamily: "'Mukta', sans-serif"};
+
+    const images = [
+        "https://ik.imagekit.io/geb6bfhmhx/fourever/page/1.jpg?updatedAt=1754481074134",
+        "https://ik.imagekit.io/geb6bfhmhx/fourever/page/2.jpg?updatedAt=1754481074008",
+        "https://ik.imagekit.io/geb6bfhmhx/fourever/page/3.jpg?updatedAt=1754481074036",
+        "https://ik.imagekit.io/geb6bfhmhx/fourever/page/4.jpg?updatedAt=1754481073919",
+        "https://ik.imagekit.io/geb6bfhmhx/fourever/page/5.jpg?updatedAt=1754481073954",
+    ]
+
+    // "https://ik.imagekit.io/geb6bfhmhx/fourever/page/order%20now.png?updatedAt=1754481073179",
+
 
     return (
         <div className="text-slate-600 pt-12 h-full w-full bg-slate-100 overflow-hidden">
+
             <_BuildHead/>
 
-            <Fade cascade className="flex flex-col items-center max-w-xl mx-auto">
+            <div className="flex flex-col items-center max-w-xl mx-auto">
 
                 <section className="text-6xl pb-6" style={primaryFontFamily}>
                     <h1 className="z-10">Fourever Gift</h1>
-                    <img src={imageUrlList[6]} className="h-auto w-28 absolute left-0 top-0" alt="frame"/>
+
                 </section>
 
-                <Fade
-                    cascade
-                    damping={0.75}
-                    triggerOnce
-                    className="flex flex-col gap-10 py-6 0 relative">
-
-                    <SideBySideSectionItem
-                        imageUrl={imageUrlList[0]}
-                        title={"Acrylic Color"}
-                        description={[
-                            "🌸 Ukuran 6R ( 15cm x 21cm )",
-                            "🌸 Sketsa 100% Handmade",
-                            "🌸 Bisa Request Sketsa dari foto / tulisan",
-                            "🌸 Free Packaging Box Aesthetic + Stiker + Greeting Card",
-                            "🌸 Khusus Paket dengan bunga, Gratis bunga hiasan random",
-                            "🌸 Khusus Paket Full Flower, Gratis Snack / mini bouqet random",
-                            "🌸 Pengerjaan 2-3 Hari",
-                        ]}
-                        price={"Rp. 120.000"}
-                        decoration={
-                            <img
-                                src={imageUrlList[4]}
-                                className="h-auto w-8 absolute -right-4 opacity-50 animate-bounce"
-                                alt="frame"/>
-                        }
-                    />
-
-                    <SideBySideSectionItem
-                        imageUrl={imageUrlList[1]}
-                        title={"Acrylic LED"}
-                        description={[
-                            "🌸 Ukuran 6R ( 15cm x 21cm )",
-                            "🌸 Stand kayu LED",
-                            "🌸 Sketsa 100% Handmade",
-                            "🌸 Bisa Request Sketsa dari foto / tulisan",
-                            "🌸 Free Packaging Box Aesthetic + Stiker + Greeting Card",
-                            "🌸 Khusus Paket dengan bunga, Gratis bunga hiasan random",
-                            "🌸 Khusus Paket Full Flower, Gratis Snack / mini bouqet random",
-                            "🌸 Pengerjaan 1-3 Hari",
-                        ]}
-                        price={"Rp. 95.000"}
-                        decoration={
-                            <img
-                                src={imageUrlList[5]}
-                                className="h-auto w-20 absolute -right-9 bottom-10 opacity-50 animate-bounce delay-1000"
-                                alt="frame"/>
-                        }
-                    />
-
-                    <SideBySideSectionItem
-                        imageUrl={imageUrlList[2]}
-                        title={"Giftbox"}
-                        description={[
-                            "🌸 Bisa custom sesuai keinginan",
-                            "🌸 Boleh kunjungi akun @fourevershopp untuk lihat rekomendasi barang yg mau diisi",
-                            "🌸 Bisa custom snack",
-                            "🌸 Untuk Snack bisa disesuaikan dengan budget",
-                            "🌸 Untuk harga sesuai isi dalam box sudah termasuk box dan greeting card",
-                        ]}
-                        price={""}
-                        decoration={
-                            <img
-                                src={imageUrlList[7]}
-                                className="h-auto w-28 absolute right-10 -bottom-10 opacity-50 animate-bounce"
-                                alt="frame"/>
-                        }
-                    />
-
-                    <SideBySideSectionItem
-                        imageUrl={imageUrlList[3]}
-                        title={"Bunga Bouquet"}
-                        description={[
-                            "🌸 Ukuran S Rp. 65.000",
-                            "🌸 Ukuran M Rp. 115.000",
-                            "🌸 Ukuran L Rp. 165.000",
-                        ]}
-                        price={""}
-                        decoration={
-                            <img
-                                src={imageUrlList[6]}
-                                className="h-auto w-28 absolute -right-1 -bottom-10 "
-                                alt="frame"/>
-                        }
-                    />
-
-                </Fade>
                 <section>
+                    {images.map((e, index) =>
+                        <div key={index} className="relative">
+                            <img src={e} alt={"Images " + index}/>
+                            {index !== 0  &&
+                                <div className={"absolute left-0 bottom-[26%] top-[50%] right-0 w-full "}>
+                                    <div className=" h-full flex flex-col  justify-center items-end gap-[60%] pr-[16%]">
+                                        <a className="w-[15%]"
+                                           target="_blank" rel="noopener noreferrer"
+                                           href="https://api.whatsapp.com/send/?phone=%2B6287865938722&text&type=phone_number">
+                                            <img src="https://ik.imagekit.io/geb6bfhmhx/fourever/page/order%20now.png?updatedAt=1754481073179" alt="buy"/>
+                                        </a>
+                                        <a className="w-[15%]"
+                                           target="_blank" rel="noopener noreferrer"
+                                           href="https://api.whatsapp.com/send/?phone=%2B6287865938722&text&type=phone_number">
+                                            <img src="https://ik.imagekit.io/geb6bfhmhx/fourever/page/order%20now.png?updatedAt=1754481073179" alt="buy"/>
+                                        </a>
+                                    </div>
+                                </div>}
+                        </div>)}
+                </section>
+
+                <section className="w-full">
                     <div
-                        className="py-4 from-sky-200/50 via-sky-50 to-transparent bg-gradient-to-b  w-screen flex flex-col justify-center items-center ">
+                        className="py-4 flex flex-col justify-center items-center ">
                         <div style={secondaryFontFamily}
                              className="text-4xl pb-3">
                             Fourever Gift
                         </div>
                         <Fade
                             cascade
-                            style={thirtiaryFontFamily}
+                            style={thirdFontFamily}
                             className="w-3/4 flex flex-col max-w-xs">
 
                             <_ClosingListItem
@@ -219,7 +118,7 @@ export default function Fourever() {
                             />
 
                             <_ClosingListItem
-                                title="0878 6593 8722"
+                                title="0878-6593-8722"
                                 icon={<i className="lab la-whatsapp text-lg"/>}
                                 link={"https://api.whatsapp.com/send/?phone=%2B6287865938722&text&type=phone_number"}
                             />
@@ -228,17 +127,11 @@ export default function Fourever() {
                     </div>
                 </section>
 
-                {/*<section className="py-6">*/}
-                {/*    <div className="h-40 w-screen bg-blue-100 ">*/}
-
-                {/*        Image divider goes here*/}
-                {/*    </div>*/}
-                {/*</section>*/}
 
                 <section className="py-6 animate-pulse">
                     <FooterDefault/>
                 </section>
-            </Fade>
+            </div>
         </div>
     );
 }
